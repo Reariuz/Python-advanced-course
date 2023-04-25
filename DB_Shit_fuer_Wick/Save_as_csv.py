19ba5530d0981d383afbce8f00375a7deeba7bb6
Reariuz <118771123+Reariuz@users.noreply.github.com>
2023-04-25 14:02:32 +0000

    fh

--- DB_Shit_fuer_Wick/Save_as_csv.py
+++ DB_Shit_fuer_Wick/Save_as_csv.py
@@ -2,16 +2,29 @@ import sqlite3
 import os.path
 import csv
 
+# BASE_DIR ist eine variable die den dateifad dieser datei enthält
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# db_path ist der zusammengefügte pfad betriebsystemgerecht.
 db_path = os.path.join(BASE_DIR, "finance.db")
+
+#verbinde mit db
 with sqlite3.connect(db_path) as db:
-#   conn = sqlite3.connect("finance.db")
-    db.text_factory = str ## my current (failed) attempt to resolve this
+
+    #setzt cursor auf db
     cur = db.cursor()
+
+    #Filter db per SQL string und speicher ergebniss
     data = cur.execute("SELECT * FROM Portfolio")
 
+#öffne eine output.csv, wenn noch nicht da, erstelle mit schreibrechten und lege den handler in f ab
 with open('output.csv', 'w', newline='') as f:
+
+    #erstelle einen csv-schreiber in writer auf f
     writer = csv.writer(f)
-    #writer.writerow(['Column 1', 'Column 2', ...])
+
+    # schreibe die daten zeilenweise
     writer.writerows(data)
+
+#schließe den shit
 f.close()
